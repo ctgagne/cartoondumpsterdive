b5c5c82de5ffdbf5459f91c1951b985b6161864f
Chaz Gagne <chazgagne@gmail.com>
2020-08-22 17:51:24 +0200

    about me done

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -27,6 +27,15 @@
         </svg>
       </div>
     </a>
+    <a href="https://www.twitch.tv/cartoondumpsterdive">
+      <div class="social-button">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z"
+          />
+        </svg>
+      </div>
+    </a>
   </div>
 </template>
 
@@ -70,6 +79,7 @@ export default {
   background-color: #5f3a5b;
   border-radius: 4px;
   margin: 0.5rem;
+  padding: 0.5rem;
 }
 
 #social a {
